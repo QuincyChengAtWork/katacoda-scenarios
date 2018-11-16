84759da2b791de214676bef6cbe8a3cbc82c125e
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-16 11:50:49 +0800

    Update env-init.sh

--- conjur-oss-on-docker/env-init.sh
+++ conjur-oss-on-docker/env-init.sh
@@ -1,7 +1,7 @@
-clear && echo "[ Getting your environment ready.  It will take a few minutes ]"
+echo "[ Getting your environment ready.  It will take a few minutes ]"
 
 docker pull postgres:9.3
 docker pull conjurinc/cli5
 docker pull cyberark/conjur
 
-clear && echo "[ System ready! Let's get started ]"
+echo "[ System ready! Let's get started ]"
