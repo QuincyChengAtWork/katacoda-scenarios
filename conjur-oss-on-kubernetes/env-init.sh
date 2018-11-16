aae1b16647cd7855d7ce4cd8ce938d634c7c5f9b
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-16 16:24:44 +0800

    Create env-init.sh

---
+++ conjur-oss-on-kubernetes/env-init.sh
@@ -0,0 +1,7 @@
+https://github.com/cyberark/kubernetes-conjur-deploy.git
+
+#docker pull postgres:9.3
+#docker pull conjurinc/cli5
+#docker pull cyberark/conjur
+
+clear && printf "\033[0;32mEnvironment ready! Let's get started.\033[0m\n"
