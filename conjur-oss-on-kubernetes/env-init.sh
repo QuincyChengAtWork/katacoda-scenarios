a966999583f8c9f1cb913a5f0394e9ca4aada37c
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-18 12:35:52 +0800

    Update env-init.sh

--- conjur-oss-on-kubernetes/env-init.sh
+++ conjur-oss-on-kubernetes/env-init.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/cyberark/secretless-broker.git
+#git clone https://github.com/cyberark/secretless-broker.git
 
 docker pull cyberark/conjur:latest
 docker pull cyberark/conjur-cli:5
-docker pull postgres:9.6
-docker pull cyberark/demo-app:latest
-docker pull cyberark/secretless-broker:latest
+#docker pull postgres:9.6
+#docker pull cyberark/demo-app:latest
+#docker pull cyberark/secretless-broker:latest
 
 
 #git clone https://github.com/cyberark/kubernetes-conjur-deploy.git
