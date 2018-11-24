cebce85d2782c4618ac09ba03c78666a5b703e13
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-25 01:14:43 +0800

    Update env-init.sh

--- conjur-cdemo/env-init.sh
+++ conjur-cdemo/env-init.sh
@@ -1,5 +1,18 @@
 git clone https://github.com/conjurdemos/cdemo.git
 
 echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
+
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt-get -y update
+
+apt-get remove docker docker-engine docker.io -y
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
