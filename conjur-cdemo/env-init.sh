a1c1d8c03d7f8a0299ff376d7f9fe90325c2cb8b
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-24 15:52:37 +0800

    Update env-init.sh

--- conjur-cdemo/env-init.sh
+++ conjur-cdemo/env-init.sh
@@ -1,9 +1,10 @@
 git clone https://github.com/conjurdemos/cdemo.git
 
-apt-get -y install dirmngr
+apt-get update -y
+apt-get install dirmngr -y
 echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update -y
-apt-get upgrade -y
+#apt-get upgrade -y
 
 apt-get install ansible -y
