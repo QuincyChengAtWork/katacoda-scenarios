06b04d485023b5731100c7ad0222ab1ef3dfbf45
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-11-24 16:26:59 +0800

    Update env-init.sh

--- conjur-cdemo/env-init.sh
+++ conjur-cdemo/env-init.sh
@@ -1,1 +1,5 @@
 git clone https://github.com/conjurdemos/cdemo.git
+
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get -y update
