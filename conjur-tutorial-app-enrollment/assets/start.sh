412c4d29bf346bd3a123d655737ed21adfc5a15f
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-12-08 14:21:34 +0800

    Create start.sh

---
+++ conjur-tutorial-app-enrollment/assets/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -o docker-compose.yml https://www.conjur.org/get-started/docker-compose.quickstart.yml
+docker-compose pull
+docker-compose run --no-deps --rm conjur data-key generate > data_key
+export CONJUR_DATA_KEY="$(< data_key)"
+docker-compose up -d
+sleep 10
+docker-compose exec conjur conjurctl account create test | tee test.out
+api_key="$(grep API test.out | cut -d: -f2 | tr -d ' \r\n')"
+docker-compose exec client bash -c "echo yes | conjur init -u https://conjur -a test"
+docker-compose exec client conjur authn login -u admin -p "$api_key"
