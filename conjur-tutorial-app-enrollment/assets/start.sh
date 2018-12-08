bb1120823d356dc62ccb29e22bbdd0fc90b1de5f
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-12-08 21:22:03 +0800

    Update start.sh

--- conjur-tutorial-app-enrollment/assets/start.sh
+++ conjur-tutorial-app-enrollment/assets/start.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 curl -o docker-compose.yml https://www.conjur.org/get-started/docker-compose.quickstart.yml
-docker-compose pull database conjur
+docker-compose pull
 docker-compose run --no-deps --rm conjur data-key generate > data_key
 export CONJUR_DATA_KEY="$(< data_key)"
-docker-compose up -d database conjur
+
+docker-compose up -d 
 sleep 10
 docker-compose exec conjur conjurctl account create demo | tee admin.out
 api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
 
 #Install Conjur CLI
-apt-get update -y
-apt-get install -y ruby ruby-dev rubygems gcc make build-essential g++
-gem install conjur-cli
+#apt-get update -y
+#apt-get install -y ruby ruby-dev rubygems gcc make build-essential g++
+#gem install conjur-cli
 
-echo yes | conjur init -u $1 -a demo
-conjur authn login -u admin -p "$api_key"
+docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
+docker-compose exec client conjur authn login -u admin -p "$api_key"
+docker-compose exec client bash -c "mkdir /app"
 
+cat >>~/.bin/conjur<<EOF
+docker cp ~/tutorial/ tutorial_client_1:/app
+docker-compose exec client bash -c "conjur $@"
+EOF
+
+chmod +x ~/.bin/conjur
