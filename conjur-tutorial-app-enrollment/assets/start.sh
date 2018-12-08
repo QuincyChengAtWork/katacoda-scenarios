53dba72ee66ced23c5d4803915ffb1a693e46466
QuincyChengAtWork <quincy.cheng@cyberark.com>
2018-12-08 22:05:29 +0800

    Update start.sh

--- conjur-tutorial-app-enrollment/assets/start.sh
+++ conjur-tutorial-app-enrollment/assets/start.sh
@@ -9,18 +9,5 @@ sleep 10
 docker-compose exec conjur conjurctl account create demo | tee admin.out
 api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
 
-#Install Conjur CLI
-#apt-get update -y
-#apt-get install -y ruby ruby-dev rubygems gcc make build-essential g++
-#gem install conjur-cli
-
 docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
 docker-compose exec client conjur authn login -u admin -p "$api_key"
-docker-compose exec client bash -c "mkdir /app"
-
-cat >>~/.bin/conjur<<EOF
-docker cp ~/tutorial/ tutorial_client_1:/app
-docker-compose exec client bash -c "conjur $@"
-EOF
-
-chmod +x ~/.bin/conjur
