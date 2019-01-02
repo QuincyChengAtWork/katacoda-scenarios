c4d27ce88e193a4f8d1d9afc856f7ad6fbf8b524
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-01-02 20:20:55 +0800

    Update setupConjur.sh

--- conjur-envoy-proxy/assets/setupConjur.sh
+++ conjur-envoy-proxy/assets/setupConjur.sh
@@ -7,7 +7,14 @@ export CONJUR_DATA_KEY="$(< data_key)"
 docker-compose up -d 
 sleep 10
 docker-compose exec conjur conjurctl account create demo | tee admin.out
-api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
 
 docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
+
+echo "Waiting for Conjur to start."
+until $(curl --output /dev/null --silent --head --fail http://conjur); do
+    printf '.'
+    sleep 5
+done
+
+api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
 docker-compose exec client conjur authn login -u admin -p "$api_key"
