fe0bb5f119d2d5c10c1da11e08dd99fcc6204ba7
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-01-28 17:57:03 +0800

    Update courseBase.sh

--- conjur-oss-1liner/courseBase.sh
+++ conjur-oss-1liner/courseBase.sh
@@ -5,4 +5,6 @@ cat > /usr/sbin/sudo << EOF
 \$*
 EOF
 chmod +x /usr/sbin/sudo
+clear && printf "\033[0;32mSetting up the system.  Please wait...\033[0m\n"
+apt-get install dialog 
 clear && printf "\033[0;32mSystem ready! Let's get started.\033[0m\n"
