85029c79942193ed48910d0e82569fd25e5bd97d
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-01-28 17:39:56 +0800

    Update courseBase.sh

--- conjur-oss-1liner/courseBase.sh
+++ conjur-oss-1liner/courseBase.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export USER=tutorial
-cat >> /usr/sbin/sudo << EOF
+cat > /usr/sbin/sudo << EOF
 #!/bin/sh
-$*
+$@
 EOF
 chmod +x /usr/sbin/sudo
 clear && printf "\033[0;32mSystem ready! Let's get started.\033[0m\n"
