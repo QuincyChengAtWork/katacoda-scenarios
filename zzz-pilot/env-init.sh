d9c7d33fa64a71b8b50e317d0af35de5514268e0
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-02-16 22:19:30 +0800

    Create env-init.sh

---
+++ zzz-pilot/env-init.sh
@@ -0,0 +1,53 @@
+cat > conjur.bak << EOF
+- !policy
+  id: db
+
+- !policy
+  id: frontend
+EOF
+
+cat > db1.bak << EOF
+# Declare the secrets which are used to access the database
+- &variables
+  - !variable password
+
+# Define a group which will be able to fetch the secrets
+- !group secrets-users
+
+- !permit
+  resource: *variables
+  # "read" privilege allows the client to read metadata.
+  # "execute" privilege allows the client to read the secret data.
+  # These are normally granted together, but they are distinct
+  #   just like read and execute bits on a filesystem.
+  privileges: [ read, execute ]
+  roles: !group secrets-users
+EOF
+
+cat > db2.ank << EOF
+- &variables
+  - !variable password
+
+- !group secrets-users
+
+- !permit
+  resource: *variables
+  privileges: [ read, execute ]
+  roles: !group secrets-users
+
+# Entitlements
+
+- !grant
+  role: !group secrets-users
+  member: !layer /frontend
+EOF
+
+cat > frontend.bak << EOF
+- !layer
+
+- !host frontend-01
+
+- !grant
+  role: !layer
+  member: !host frontend-01
+EOF
