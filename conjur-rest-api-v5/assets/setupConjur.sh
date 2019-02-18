f6595356d60df77bc78e4413242fa6923458d9eb
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-02-18 18:48:40 +0800

    Update setupConjur.sh

--- conjur-rest-api-v5/assets/setupConjur.sh
+++ conjur-rest-api-v5/assets/setupConjur.sh
@@ -8,7 +8,8 @@ export CONJUR_DATA_KEY="$(< data_key)"
 docker-compose up -d database conjur
 sleep 10
 docker-compose exec conjur conjurctl account create demo | tee admin.out
-api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
+export conjur_admin="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
+export conjur_url=$1
 
 #docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
 #docker-compose exec client conjur authn login -u admin -p "$api_key"
