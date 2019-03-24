80b420ae28b570973663d165367ffe307e641b40
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-03-24 18:53:53 +0800

    Update env-init.sh

--- conjur-oss-on-kubernetes/env-init.sh
+++ conjur-oss-on-kubernetes/env-init.sh
@@ -9,7 +9,8 @@
 
 #git clone https://github.com/cyberark/kubernetes-conjur-deploy.git
 #docker pull postgres:9.3
+#clear && printf "\033[0;32mEnvironment ready! Let's get started.\033[0m\n"
+
 
 launch.sh
 
-clear && printf "\033[0;32mEnvironment ready! Let's get started.\033[0m\n"
