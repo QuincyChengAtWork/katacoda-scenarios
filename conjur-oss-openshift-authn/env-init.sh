83b18cbeb4bccc24de353bd627c2e750bc722048
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-05-09 18:10:51 +0800

    Update env-init.sh

--- conjur-oss-openshift-authn/env-init.sh
+++ conjur-oss-openshift-authn/env-init.sh
@@ -3,7 +3,3 @@ ssh root@host01 'oc adm policy add-cluster-role-to-group sudoer system:authentic
 ssh root@host01 "~/.setup-volumes"
 ssh root@host01 "~/.setup-templates"
 
-git clone https://github.com/QuincyChengAtWork/conjur-oss-openshift-authn-katacoda.git
-cd conjur-oss-openshift-authn-katacoda
-clear
-launch.sh
