d67e7562a499ce99fef624c7c13c468d3a1d6431
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-05-21 09:31:22 +0800

    Update env-init.sh

--- terraform-provider-conjur/env-init.sh
+++ terraform-provider-conjur/env-init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "Preparing the environment.  It will take a few moments."
+
 cd /root
 curl -sO https://raw.githubusercontent.com/QuincyChengAtWork/katacoda-scenarios/master/terraform-provider-conjur/assets/conjur.tf
 curl -sO https://raw.githubusercontent.com/QuincyChengAtWork/katacoda-scenarios/master/terraform-provider-conjur/assets/conjur.yml
@@ -7,3 +10,9 @@ curl -sO https://raw.githubusercontent.com/QuincyChengAtWork/katacoda-scenarios/
 curl -sO https://raw.githubusercontent.com/QuincyChengAtWork/katacoda-scenarios/master/terraform-provider-conjur/assets/secrets.yml
 curl -sO https://raw.githubusercontent.com/QuincyChengAtWork/katacoda-scenarios/master/terraform-provider-conjur/assets/setupConjur.sh
 chmod +x setupConjur.sh
+
+apt-get update
+apt-get install unzip
+
+
+clear && echo "The environment is now ready!"
