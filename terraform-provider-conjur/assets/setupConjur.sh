f27327e78743431340d609d7f90dda3e1acacba4
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-05-23 17:31:32 +0800

    Update setupConjur.sh

--- terraform-provider-conjur/assets/setupConjur.sh
+++ terraform-provider-conjur/assets/setupConjur.sh
@@ -13,9 +13,9 @@ conjur_ip="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}
 docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
 docker-compose exec client conjur authn login -u admin -p "$api_key"
 
-#export CONJUR_APPLIANCE_URL=http://$conjur_ip
-export CONJUR_APPLIANCE_URL=$1
-
+export CONJUR_APPLIANCE_URL=${1%/}
 export CONJUR_ACCOUNT=demo
 export CONJUR_AUTHN_LOGIN=admin
 export CONJUR_AUTHN_API_KEY="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
+export CONJUR_MAJOR_VERSION=5
+export CONJUR_VERSION=5
