ccdaf4365f87750baad2aa8b4c9273de2e68c44e
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-06-29 11:32:00 +0800

    Update setupConjur.sh

--- conjur-ansible-ssh/assets/setupConjur.sh
+++ conjur-ansible-ssh/assets/setupConjur.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+sudo service docker restart
 curl -o docker-compose.yml https://quincycheng.github.io/docker-compose.quickstart.yml
-git clone https://github.com/cyberark/conjur-quickstart.git
-cd conjur-quickstart
 docker-compose pull
 docker-compose run --no-deps --rm conjur data-key generate > data_key
 export CONJUR_DATA_KEY="$(< data_key)"
@@ -12,4 +11,3 @@ docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
 sleep 10
 api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
 docker-compose exec client conjur authn login -u admin -p "$api_key"
-cd ..
