03702c0f16cf853867e8a9f129a8064e7a96fab4
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-07-03 11:15:18 +0800

    Update env-init.sh

--- conjur-oss-k8s-authn/env-init.sh
+++ conjur-oss-k8s-authn/env-init.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/QuincyChengAtWork/conjur-oss-k8s-authn-katacoda.git
 #cd conjur-oss-k8s-authn-katacoda
 
-wget https://github.com/QuincyChengAtWork/conjur-oss-openshift-authn-katacoda/archive/v1.0.zip
+wget https://github.com/QuincyChengAtWork/conjur-oss-k8s-authn-katacoda/archive/v1.0.zip
 unzip v1.0.zip
-cd conjur-oss-openshift-authn-katacoda-1.0/
+cd conjur-oss-k8s-authn-katacoda-1.0/
 
 clear
