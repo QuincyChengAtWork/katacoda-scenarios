e5a84fa723f9dfa477c4660104d6125eead6453f
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-07-08 01:46:29 +0800

    Update env-init.sh

--- conjur-oss-k8s-authn/env-init.sh
+++ conjur-oss-k8s-authn/env-init.sh
@@ -3,6 +3,13 @@ git clone https://github.com/QuincyChengAtWork/conjur-oss-k8s-authn-katacoda.git
 
 wget https://github.com/QuincyChengAtWork/conjur-oss-k8s-authn-katacoda/archive/v1.0.zip
 unzip v1.0.zip
+
+cp conjur-oss-k8s-authn-katacoda/policy/host-policy.yml conjur-oss-k8s-authn-katacoda-1.0/policy/
+cp conjur-oss-k8s-authn-katacoda/policy/host-entitlement.yml conjur-oss-k8s-authn-katacoda-1.0/policy/
+
+cp conjur-oss-k8s-authn-katacoda/test-app/secretless.yml conjur-oss-k8s-authn-katacoda-1.0/test-app/
+cp conjur-oss-k8s-authn-katacoda/test-app/manifest-secretless.yml conjur-oss-k8s-authn-katacoda-1.0/test-app/
+
 cd conjur-oss-k8s-authn-katacoda-1.0/
 
 clear
