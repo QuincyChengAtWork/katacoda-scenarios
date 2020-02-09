da21911ebffa66feefb0e9ca0eef56354eb921e7
QuincyChengAtWork <quincy.cheng@cyberark.com>
2020-02-09 23:31:01 +0800

    Create setupConjur.sh

---
+++ java-conjur-summon/assets/setupConjur.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -o docker-compose.yml https://quincycheng.github.io/docker-compose.quickstart.yml
+docker-compose pull
+docker-compose run --no-deps --rm conjur data-key generate > data_key
+export CONJUR_DATA_KEY="$(< data_key)"
+
+docker-compose up -d 
+sleep 10
+docker-compose exec conjur conjurctl account create demo | tee admin.out
+api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
+
+docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
+docker-compose exec client conjur authn login -u admin -p "$api_key"
