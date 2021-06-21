3c65adf33d36b656dd150a1329201e293d7eca7a
QuincyChengAtWork <quincy.cheng@cyberark.com>
2021-06-21 18:36:08 +0800

    Update conjur.tf

--- terraform-provider-conjur/assets/conjur.tf
+++ terraform-provider-conjur/assets/conjur.tf
@@ -1,3 +1,16 @@
+terraform {
+ required_providers {
+   conjur = {
+     source  = "local/cyberark/conjur"
+     version = "0.5.0"
+   }
+   docker = {
+     source  = "kreuzwerker/docker"
+     version = "2.11.0"
+   }
+ }
+}
+
 provider "conjur" {}
 
 data "conjur_secret" "admin-password" {
