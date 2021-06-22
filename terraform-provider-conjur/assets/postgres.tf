cad50eef103a4d4e017c0cb5e7fbdf88cf3c6bbe
QuincyChengAtWork <quincy.cheng@cyberark.com>
2021-06-22 13:46:46 +0800

    Update postgres.tf

--- terraform-provider-conjur/assets/postgres.tf
+++ terraform-provider-conjur/assets/postgres.tf
@@ -10,7 +10,9 @@ resource "docker_container" "postgres" {
   name = "postgres"
   image = "${docker_image.postgres.latest}"
   env = ["POSTGRES_PASSWORD=${data.conjur_secret.admin-password.value}"]
-  networks = ["${docker_network.demo.name}"]
+  networks_advanced  {
+    name="${docker_network.demo.name}" 
+  }
   ports {
     internal = 5432
     external = 5432
