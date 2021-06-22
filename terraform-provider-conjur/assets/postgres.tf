c171e2780844e5244c7ac8a8be7bac1fe874ef8d
QuincyChengAtWork <quincy.cheng@cyberark.com>
2021-06-22 09:49:41 +0800

    Update postgres.tf

--- terraform-provider-conjur/assets/postgres.tf
+++ terraform-provider-conjur/assets/postgres.tf
@@ -1,3 +1,11 @@
+data "conjur_secret" "admin-password" {
+   name = "postgres/admin-password"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:9.3"
+}
+
 resource "docker_container" "postgres" {
   name = "postgres"
   image = "${docker_image.postgres.latest}"
@@ -9,6 +17,3 @@ resource "docker_container" "postgres" {
   }
 }
 
-resource "docker_image" "postgres" {
-  name = "postgres:9.3"
-}
